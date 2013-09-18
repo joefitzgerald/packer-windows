c7446a4318cda6bc02218293968810e267555123
Joe Fitzgerald <joeyfitz@gmail.com>
2013-09-18 10:17:14 -0600

    VMware Updates, Reformatting JSON
    - Updating GuestOS value for Windows Server 2018 to windows8srv-64
    - Setting headless to true for all provisioners
    - Updating VMware Tools to latest version

--- scripts/install-vmware-tools.sh
+++ scripts/install-vmware-tools.sh
@@ -11,21 +11,21 @@ msiexec /qb /i 7z920-x64.msi
 mkdir /home/vagrant/vmware
 chown -R vagrant /home/vagrant/vmware
 cd /home/vagrant/vmware
-wget http://softwareupdate.vmware.com/cds/vmw-desktop/ws/9.0.2/1031769/windows/packages/tools-windows-9.2.3.exe.tar
-tar -xvf /home/vagrant/vmware/tools-windows-9.2.3.exe.tar
-rm /home/vagrant/vmware/tools-windows-9.2.3.exe.tar
+wget http://softwareupdate.vmware.com/cds/vmw-desktop/ws/10.0.0/1295980/windows/packages/tools-windows-9.6.0.exe.tar
+tar -xvf /home/vagrant/vmware/tools-windows-9.6.0.exe.tar
+rm -rf /home/vagrant/vmware/tools-windows-9.6.0.exe.tar
 chown -R vagrant /home/vagrant/vmware
 
 # Install the VMware Tools
-/home/vagrant/vmware/tools-windows-9.2.3.exe
-rm /home/vagrant/vmware/tools-windows-9.2.3.exe
+/home/vagrant/vmware/tools-windows-9.6.0.exe
+rm -rf /home/vagrant/vmware/tools-windows-9.6.0.exe
 /cygdrive/c/Program\ Files/7-Zip/7z.exe x C:\\Program\ Files\ \(x86\)\\VMware\\tools-windows\\windows.iso
-rm C:\\Program\ Files\ \(x86\)\\VMware\\tools-windows\\windows.iso
+rm -rf C:\\Program\ Files\ \(x86\)\\VMware\\tools-windows\\windows.iso
 chown -R vagrant /home/vagrant/vmware
 cmd.exe /c "C:\\cygwin\\home\\vagrant\\vmware\\setup.exe /S /v\"/qn REBOOT=R\""
 rm -rf /home/vagrant/vmware
 cd /home/vagrant
 msiexec /qb /x 7z920-x64.msi
-rm 7z920-x64.msi
+rm -rf 7z920-x64.msi
 
 sleep 1
