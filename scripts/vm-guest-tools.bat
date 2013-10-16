93027f3f93b0e48d7c86f0953b03e9cd8386f218
dylanmei <dylanmei@gmail.com>
2013-10-16 10:46:37 -0700

    Escape parens when installing vmware-tools

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -13,7 +13,7 @@ if not exist "C:\Windows\Temp\VMWare\setup.exe" (
   powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://softwareupdate.vmware.com/cds/vmw-desktop/ws/10.0.0/1295980/windows/packages/tools-windows-9.6.0.exe.tar', 'C:\Windows\Temp\vmware-tools.exe.tar')" <NUL
   cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\vmware-tools.exe.tar -oC:\Windows\Temp"
   cmd /c C:\Windows\Temp\tools-windows-9.6.0
-  cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\Program Files (x86)\VMWare\tools-windows\windows.iso" -oC:\Windows\Temp\VMWare"
+  cmd /c ""C:\Program Files\7-Zip\7z.exe" x "C:\Program Files ^(x86^)\VMWare\tools-windows\windows.iso" -oC:\Windows\Temp\VMWare"
   del /F /S /Q "C:\Program Files (x86)\VMWare"
 )
 
