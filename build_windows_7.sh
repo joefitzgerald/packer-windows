9d0c5dcdc1b94b7df52c7579637b36e4a83a3d2f
Stefan Scherer <stefan@Stefans-MacBook-Pro.local>
2014-03-08 11:00:53 +0100

    scripted build steps

---
+++ build_windows_7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -f windows_7_virtualbox.box ]; then
+  rm windows_7_virtualbox.box
+fi
+
+packer build -only=virtualbox-iso windows_7.json
+
+if [ -f windows_7_virtualbox.box ]; then
+  vagrant box remove windows_7
+  vagrant box add windows_7 windows_7_virtualbox.box 
+fi
