5a366aaf2134400ab08df629ea9ee27dd86a26a6
Stefan Scherer <stefan@Stefans-MacBook-Pro.local>
2014-03-08 10:22:49 +0100

    scripted build steps

---
+++ build_windows_2012_r2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -f windows_2012_r2_virtualbox.box ]; then
+  rm windows_2012_r2_virtualbox.box
+fi
+
+packer build -only=virtualbox-iso windows_2012_r2.json
+
+if [ -f windows_2012_r2_virtualbox.box ]; then
+  vagrant box remove windows_2012_r2
+  vagrant box add windows_2012_r2 windows_2012_r2_virtualbox.box 
+fi
