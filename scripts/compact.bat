3ee3d3cd93affef42d49fdef3ca5ac7caaaf6029
Jeff Waugh <jdub@bethesignal.org>
2014-05-05 18:47:01 +1000

    make sure we have 7zip in compact.bat

--- scripts/compact.bat
+++ scripts/compact.bat
@@ -1,3 +1,6 @@
+if not exist "C:\Windows\Temp\7z920-x64.msi" (
+	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/sevenzip/7z920-x64.msi', 'C:\Windows\Temp\7z920-x64.msi')" <NUL
+)
 msiexec /qb /i C:\Windows\Temp\7z920-x64.msi
 
 if not exist "C:\Windows\Temp\ultradefrag.zip" (
