7d37d3159483c74f21e760f85c23ce7ed170f69a
Ken Sykora <ksykora@nerdery.com>
2014-06-25 08:22:43 -0500

    Updated puppet version to latest

--- scripts/puppet.bat
+++ scripts/puppet.bat
@@ -1,5 +1,5 @@
 if not exist "C:\Windows\Temp\puppet.msi" (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.puppetlabs.com/windows/puppet-3.5.1.msi', 'C:\Windows\Temp\puppet.msi')" <NUL
+  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://downloads.puppetlabs.com/windows/puppet-3.6.2.msi', 'C:\Windows\Temp\puppet.msi')" <NUL
 )
 
 :: http://docs.puppetlabs.com/pe/latest/install_windows.html
