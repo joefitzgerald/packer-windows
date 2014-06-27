af29823b895b485425d8a6fa2c9e7afc245ab842
StefanScherer <scherer_stefan@icloud.com>
2014-06-27 16:51:17 +0200

    moved all files to my_vagrant_vcloud branch

--- build_windows_7_vcloud.bat
+++
@@ -1,6 +0,0 @@
-if exist windows_7_vcloud.box (
-  del /F windows_7_vcloud.box
-)
-
-packer build -only=vmware-iso windows_7_vcloud.json
-
--- build_windows_81_vcloud.bat
+++
@@ -1,6 +0,0 @@
-if exist windows_81_vcloud.box (
-  del /F windows_81_vcloud.box
-)
-
-packer build -only=vmware-iso windows_81_vcloud.json
-
