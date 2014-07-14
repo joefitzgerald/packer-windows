933f1064e0e682f58ec939aee08b2aa53904613e
Dylan Meissner <dylanmei@gmail.com>
2014-07-14 12:34:42 -0700

    Update chocolatey scripts with 0.9.8.27's default install location.

--- scripts/chocolatey.bat
+++ scripts/chocolatey.bat
@@ -1,5 +1,5 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" <NUL
 
-<nul set /p ".=;C:\Chocolatey\bin" >> C:\Windows\Temp\PATH
+<nul set /p ".=;%ALLUSERSPROFILE%\chocolatey\bin" >> C:\Windows\Temp\PATH
 set /p PATH=<C:\Windows\Temp\PATH
 setx PATH "%PATH%" /m
