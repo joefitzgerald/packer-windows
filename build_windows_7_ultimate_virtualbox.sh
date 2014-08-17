be92364c5f1c818974a49e1caedcacd9e8290193
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-17 09:57:52 +0200

    more build scripts

--- build_windows_7_ultimate_virtualbox.sh
+++ build_windows_7_ultimate_virtualbox.sh
@@ -7,7 +7,6 @@ fi
 packer build -only=virtualbox-iso windows_7_ultimate.json
 
 if [ -f windows_7_ultimate_virtualbox.box ]; then
-  vagrant box remove windows_7_ultimate -f
-  vagrant box add windows_7_ultimate windows_7_ultimate_virtualbox.box 
+  vagrant box add windows_7_ultimate windows_7_ultimate_virtualbox.box --force
   #rm windows_7_ultimate_virtualbox.box
 fi
