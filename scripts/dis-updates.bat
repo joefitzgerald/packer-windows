4a45a83dfcfc07fd5d83ee12da0160c1b674c18b
Stefan Scherer <scherer_stefan@icloud.com>
2014-10-06 17:28:04 +0200

    windows_7_ultimate build with fix for issue-83

---
+++ scripts/dis-updates.bat
@@ -0,0 +1,16 @@
+rem http://www.windows-commandline.com/disable-automatic-updates-command-line/
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
+
+rem remove optional WSUS server settings
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /f
+
+rem even harder, disable windows update service
+rem sc config wuauserv start= disabled
+rem net stop wuauserv
+
+if exist C:\Windows\Temp\win-updates.log (
+  echo Show Windows Updates log file C:\Windows\Temp\win-updates.log
+  dir C:\Windows\Temp\win-updates.log
+  type C:\Windows\Temp\win-updates.log
+  echo End of Windows Updates log file C:\Windows\Temp\win-updates.log
+)
--- scripts/microsoft-updates.bat
+++ scripts/microsoft-updates.bat
@@ -4,15 +4,6 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpd
 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v IncludeRecommendedUpdates /t REG_DWORD /d 1 /f
 
-set WSUSIP=192.168.1.200
-ping -n 1 %WSUSIP% | find "TTL"
-if not errorlevel 1 (
-  echo use local WSUS server %WSUSIP%
-  reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v WUServer /t REG_SZ /d "http://%WSUSIP%:8530" /f
-  reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v WUStatusServer /t REG_SZ /d "http://%WSUSIP%:8530" /f
-  reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v UseWUServer /t REG_DWORD /d 1 /f
-)
-
 echo Set ServiceManager = CreateObject("Microsoft.Update.ServiceManager") > A:\temp.vbs
 echo Set NewUpdateService = ServiceManager.AddService2("7971f918-a847-4430-9279-4a52d1efe18d",7,"") >> A:\temp.vbs
 
