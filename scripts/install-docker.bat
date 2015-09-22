c61948b0c65320a2c6c81917852384ad400c44d2
Stefan Scherer <scherer_stefan@icloud.com>
2015-09-22 08:34:53 +0200

    Clean up docker installation

--- scripts/install-docker.bat
+++ scripts/install-docker.bat
@@ -1,10 +1,18 @@
 rem https://msdn.microsoft.com/virtualization/windowscontainers/quick_start/inplace_setup
 powershell.exe -Command "wget -uri http://aka.ms/setupcontainers -OutFile C:\ContainerSetup.ps1"
-powershell.exe -File "C:\ContainerSetup.ps1"
+
+set WIMPATH=http://192.168.254.1:8000/ContainerOSImage
+
+if "%WIMPATH%x"=="x" (
+  powershell.exe -File "C:\ContainerSetup.ps1"
+) else (
+  powershell.exe -Command "wget -uri %WIMPATH% -OutFile C:\ContainerOSImage"
+  if exist C:\ContainerOSImage (
+    powershell.exe -File "C:\ContainerSetup.ps1" -WimPath C:\ContainerOSImage
+  ) else (
+    powershell.exe -File "C:\ContainerSetup.ps1"
+  )
+  del C:\ContainerOSImage
+)
 
 rem https://msdn.microsoft.com/virtualization/windowscontainers/quick_start/manage_docker
-
-echo ======= PAUSE ========
-ping 127.0.0.1 -n 3600 > nul
-
-echo ======= PAUSE DONE ===
--- scripts/restart-helper.bat
+++
@@ -1,2 +0,0 @@
-echo ======= Sleeping while restart ========
-ping 127.0.0.1 -n 30 > nul
