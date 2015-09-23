5d3d01868894c564d78fce62a3b9fa9d8067e863
Stefan Scherer <scherer_stefan@icloud.com>
2015-09-23 08:16:09 +0200

    Local download/shared folder

--- scripts/install-docker.bat
+++ scripts/install-docker.bat
@@ -1,18 +1,30 @@
 rem https://msdn.microsoft.com/virtualization/windowscontainers/quick_start/inplace_setup
 powershell.exe -Command "wget -uri http://aka.ms/setupcontainers -OutFile C:\ContainerSetup.ps1"
 
-rem set WIMPATH=http://192.168.254.1:8000/ContainerOSImage
+set WIMPATH=http://192.168.254.1:8000/ContainerOSImage.wim
+set LOCALWIMPATH=C:\Users\vagrant\ContainerOSImage.wim
 
 if "%WIMPATH%x"=="x" (
   powershell.exe -File "C:\ContainerSetup.ps1"
 ) else (
-  powershell.exe -Command "wget -uri %WIMPATH% -OutFile C:\ContainerOSImage"
-  if exist C:\ContainerOSImage (
-    powershell.exe -File "C:\ContainerSetup.ps1" -WimPath C:\ContainerOSImage
+  powershell.exe -Command "wget -uri %WIMPATH% -OutFile %LOCALWIMPATH%"
+)
+
+:waiting
+if not exist %LOCALWIMPATH% (
+  echo Waiting for WimPath %LOCALWIMPATH%
+  ping 127.0.0.1 -n 5 > nul
+  goto waiting
+  )
+
+if exist %LOCALWIMPATH% (
+    powershell.exe -File "C:\ContainerSetup.ps1" -WimPath %LOCALWIMPATH%
+    del %LOCALWIMPATH%
   ) else (
     powershell.exe -File "C:\ContainerSetup.ps1"
   )
-  del C:\ContainerOSImage
 )
+
+echo Done with %0
 
 rem https://msdn.microsoft.com/virtualization/windowscontainers/quick_start/manage_docker
