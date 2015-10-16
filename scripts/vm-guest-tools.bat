bf7ade5c06527a171bf392056f058242e4dce4d1
Joe Fitzgerald <jfitzgerald@pivotal.io>
2015-10-15 21:14:13 -0600

    Disable Tool Upload
    - Download Tools Instead; Tool Upload Is Flakey With WinRM
    - Re-enable Windows Updates By Default
    - Re-enable Headless By Default

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -32,9 +32,17 @@ goto :done
 :: to prevent user intervention popups which will undermine a silent installation.
 cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
 
-move /Y C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
+if exist "C:\Users\vagrant\VBoxGuestAdditions.iso" (
+    move /Y C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
+)
+
+if not exist "C:\Windows\Temp\VBoxGuestAdditions.iso" (
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.virtualbox.org/virtualbox/5.0.6/VBoxGuestAdditions_5.0.6.iso', 'C:\Windows\Temp\VBoxGuestAdditions.iso')" <NUL
+)
+
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
 cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
+rd /S /Q "C:\Windows\Temp\virtualbox"
 goto :done
 
 :parallels
@@ -42,7 +50,7 @@ if exist "C:\Users\vagrant\prl-tools-win.iso" (
 	move /Y C:\Users\vagrant\prl-tools-win.iso C:\Windows\Temp
 	cmd /C "C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\prl-tools-win.iso -oC:\Windows\Temp\parallels
 	cmd /C C:\Windows\Temp\parallels\PTAgent.exe /install_silent
-	rd /S /Q "c:\Windows\Temp\parallels"
+	rd /S /Q "C:\Windows\Temp\parallels"
 )
 
 :done
