96ef036480649ceaaf6277bb816381d1d2b2fe3d
Stefan Scherer <scherer_stefan@icloud.com>
2016-03-26 19:23:50 +0100

    Updated

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#packer build --only=vmware-iso --var iso_url=~/packer_cache/Win10_1511_English_x64.iso --var iso_checksum=875ec108288b9f581e5d8099cf0edb79f0f3e483 --var autounattend=./tmp/10/Autounattend.xml --var product_key=$(pass windows_7_prof_license) windows_10.json
+#packer build --only=vmware-iso --var iso_url=~/packer_cache/Win10_1511_English_x64.iso --var iso_checksum=875ec108288b9f581e5d8099cf0edb79f0f3e483 windows_10.json
 packer build --only=vmware-iso --var iso_url=~/packer_cache/Win10_1511_English_x64.iso --var iso_checksum=875ec108288b9f581e5d8099cf0edb79f0f3e483 --var autounattend=./tmp/10/Autounattend.xml windows_10.json
