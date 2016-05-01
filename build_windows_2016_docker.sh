4eb3d410226dd9d3e5e8df4df5803a617084f83f
Stefan Scherer <scherer_stefan@icloud.com>
2016-05-01 08:50:21 +0200

    Build VMware and VirtualBox

--- build_windows_2016_docker.sh
+++ build_windows_2016_docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 packer build --only=vmware-iso --var iso_url=~/packer_cache/14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO windows_2016_docker.json
+packer build --only=virtualbox-iso --var iso_url=~/packer_cache/14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO windows_2016_docker.json
