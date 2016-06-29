99e35135d5c7520ec93c5709afbe832113353224
Patrick Lang <patrick.lang@hotmail.com>
2016-06-28 21:28:39 -0700

    Continuing work on shorter install script, but not working yet

--- scripts/docker/add-docker-group.ps1
+++ scripts/docker/add-docker-group.ps1
@@ -2,7 +2,7 @@ net localgroup docker /add
 $username = "vagrant"
 net localgroup docker $username /add
 
-(Get-Content C:\ProgramData\docker\runDockerDaemon.cmd).replace('-H npipe://', '-G docker -H npipe://') | Set-Content C:\ProgramData\docker\runDockerDaemon.cmd
+# (Get-Content C:\ProgramData\docker\runDockerDaemon.cmd).replace('-H npipe://', '-G docker -H npipe://') | Set-Content C:\ProgramData\docker\runDockerDaemon.cmd
 
 # restart-service docker
 # logoff and logon again
