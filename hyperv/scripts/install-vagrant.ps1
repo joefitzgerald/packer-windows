08980fe4bc2a432670f5f0424d49ac3802c25cd1
Stefan Scherer <scherer_stefan@icloud.com>
2016-07-06 09:04:18 +0200

    Update install-vagrant.ps1

--- hyperv/scripts/install-vagrant.ps1
+++ hyperv/scripts/install-vagrant.ps1
@@ -1,6 +1,6 @@
 choco install -y vagrant
 
-# TODO patch Vagrant 1.8.4 as described in https://github.com/PatrickLang/packer-windows/issues/1#issuecomment-230151622
+# patch Vagrant 1.8.4 as described in https://github.com/PatrickLang/packer-windows/issues/1#issuecomment-230151622
 
 if (Test-Path c:\hashicorp\vagrant\embedded\gems\specifications\vagrant-1.8.4.gemspec) {
   Write-Host "Patching vagrant to use latest winrm-fs gem"
