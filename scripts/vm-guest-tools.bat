f7bf266b30bf9fd3c15ba4c5e77a08e46d167cce
Stefan Scherer <scherer_stefan@icloud.com>
2016-07-07 10:26:34 +0200

    Update vbox addtions 5.0.24

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -37,7 +37,7 @@ if exist "C:\Users\vagrant\VBoxGuestAdditions.iso" (
 )
 
 if not exist "C:\Windows\Temp\VBoxGuestAdditions.iso" (
-    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.virtualbox.org/virtualbox/5.0.20/VBoxGuestAdditions_5.0.20.iso', 'C:\Windows\Temp\VBoxGuestAdditions.iso')" <NUL
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://download.virtualbox.org/virtualbox/5.0.24/VBoxGuestAdditions_5.0.24.iso', 'C:\Windows\Temp\VBoxGuestAdditions.iso')" <NUL
 )
 
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
