9c83128caeb5184b423b6377bccc57b20d086585
Stefan Scherer <scherer_stefan@icloud.com>
2016-07-07 20:37:49 +0200

    Cleanup

--- scripts/chocolatey.bat
+++ scripts/chocolatey.bat
@@ -1,5 +1,1 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" <NUL
-
-<nul set /p ".=;%ALLUSERSPROFILE%\chocolatey\bin" >> C:\Windows\Temp\PATH
-set /p PATH=<C:\Windows\Temp\PATH
-setx PATH "%PATH%" /m
