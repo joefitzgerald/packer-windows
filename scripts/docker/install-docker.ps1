f6d8b33c63ba4d5f22b977de38372aeb4a3a1b3a
Patrick Lang <patrick.lang@hotmail.com>
2016-08-03 11:07:07 -0700

    Switching to Docker v1.12, docker pull

--- scripts/docker/install-docker.ps1
+++ scripts/docker/install-docker.ps1
@@ -1,23 +1,19 @@
 Set-ExecutionPolicy Bypass -scope Process
-New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
-wget -outfile $env:TEMP\docker-1.12.0.zip https://get.docker.com/builds/Windows/x86_64/docker-1.12.0.zip
-Expand-Archive -Path $env:TEMP\docker-1.12.0.zip -DestinationPath $env:TEMP -Force
-copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
-Remove-Item $env:TEMP\docker-1.12.0.zip
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
-$env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
-. dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
-
+Invoke-WebRequest "https://get.docker.com/builds/Windows/x86_64/docker-1.12.0.zip" -OutFile "$env:TEMP\docker-1.12.0.zip" -UseBasicParsing
+Expand-Archive -Path "$env:TEMP\docker-1.12.0.zip" -DestinationPath $env:ProgramFiles
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:ProgramFiles\docker\", [EnvironmentVariableTarget]::Machine)
+. 'C:\Program Files\docker\dockerd' --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
 Start-Service Docker
 
-Write-Host "Installing WindowsServerCore docker image..."
-docker pull microsoft/windowsservercore:10.0.14300.1030
-docker pull microsoft/windowsservercore
+Write-Host "Installing WindowsServerCore container image..."
+& "C:\Program Files\docker\docker.exe" pull microsoft/windowsservercore:10.0.14300.1030
+& "C:\Program Files\docker\docker.exe" tag microsoft/windowsservercore:10.0.14300.1030 microsoft/windowsservercore:latest
 
 if ((get-windowsfeature Hyper-V | where installed).count) {
-  Write-Host "Installing NanoServer docker image..."
-  docker pull microsoft/nanoserver:10.0.14300.1030
-  docker pull microsoft/nanoserver
+  Write-Host "Installing NanoServer container image..."
+  & "C:\Program Files\docker\docker.exe" pull microsoft/nanoserver:10.0.14300.1030
+  & "C:\Program Files\docker\docker.exe" tag microsoft/nanoserver:10.0.14300.1030 microsoft/windowsservercore:latest
+
 } else {
-  Write-Host "Skipping NanoServer docker image"
+  Write-Host "Skipping NanoServer container image"
 }
