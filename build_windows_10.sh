5a77eeda31ad9ccd9206c196952c5116944c7f2b
Stefan Scherer <scherer_stefan@icloud.com>
2016-08-11 16:32:19 +0200

    Update my build script

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#packer build --only=vmware-iso --var iso_url=~/packer_cache/Win10_1511_English_x64.iso --var iso_checksum=875ec108288b9f581e5d8099cf0edb79f0f3e483 windows_10.json
+#packer build --only=vmware-iso windows_10.json
 
 # my own license
-# packer build --only=vmware-iso --var iso_url=~/packer_cache/Win10_1511_English_x64.iso --var iso_checksum=875ec108288b9f581e5d8099cf0edb79f0f3e483 --var autounattend=./tmp/10/Autounattend.xml windows_10.json
-
-# latest Insider ISO
-packer build --only=vmware-iso --var iso_url=~/packer_cache/Windows10_InsiderPreview_Client_x64_en-us_14295.1000.iso --var iso_checksum=ee3b237cbfff293c2c9e5b036a0250b39c3f79ae --var autounattend=./tmp/10/Autounattend.xml windows_10.json
+packer build --only=vmware-iso --var iso_url=~/packer_cache/Win10_1607_English_x64.iso --var iso_checksum=99fd8082a609997ae97a514dca22becf20420891 --var autounattend=./tmp/10/Autounattend.xml windows_10.json
