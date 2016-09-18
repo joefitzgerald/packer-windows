05a7bedc50a63e72cb155884abf5a486d9de640b
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-18 11:56:30 +0200

    Skip KB3188966 and KB3189866 for now, would hang forever

--- scripts/win-updates.ps1
+++ scripts/win-updates.ps1
@@ -67,6 +67,10 @@ function Install-WindowsUpdates() {
             [bool]$addThisUpdate = $false
             if ($Update.InstallationBehavior.CanRequestUserInput) {
                 LogWrite "> Skipping: $($Update.Title) because it requires user input"
+            } elseif ($Update.Title.Contains("KB3188966")) {
+                LogWrite "> Skipping: $($Update.Title) because it will hang forever"
+            } elseif ($Update.Title.Contains("KB3189866")) {
+                LogWrite "> Skipping: $($Update.Title) because it will hang forever"
             } else {
                 if (!($Update.EulaAccepted)) {
                     LogWrite "> Note: $($Update.Title) has a license agreement that must be accepted. Accepting the license."
@@ -125,7 +129,7 @@ function Install-WindowsUpdates() {
         LogWrite 'No updates available to install...'
         $global:MoreUpdates=0
         $global:RestartRequired=0
-        Invoke-Expression "a:\openssh.ps1 -AutoStart"
+        Invoke-Expression "a:\enable-winrm.ps1"
         break
     }
 
