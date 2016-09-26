078c8dfafa2938e0af2aa311d14d80c0318359bd
Stefan Scherer <scherer_stefan@icloud.com>
2016-09-26 18:58:59 +0200

    Update Windows Server 2016 Eval ISO

--- build_windows_2016_docker.sh
+++ build_windows_2016_docker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~/packer_cache/14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO windows_2016_docker.json
-packer build --only=virtualbox-iso --var iso_url=~/packer_cache/14300.1000.160324-1723.RS1_RELEASE_SVC_SERVER_OEMRET_X64FRE_EN-US.ISO windows_2016_docker.json
+packer build --only=vmware-iso --var iso_url=~/packer_cache/14393.0.160715-1616.RS1_RELEASE_SERVER_EVAL_X64FRE_EN-US.ISO windows_2016_docker.json
