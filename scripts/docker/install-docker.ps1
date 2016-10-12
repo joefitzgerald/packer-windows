1029c5c71a08a1e95f762f26a958e40ac684585f
Stefan Scherer <scherer_stefan@icloud.com>
2016-10-12 16:06:26 +0200

    Update docker link to final CS version

--- scripts/docker/install-docker.ps1
+++ scripts/docker/install-docker.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -scope Process
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
-wget -outfile $env:TEMP\docker-cs-1.12.zip "https://download.docker.com/components/engine/windows-server/cs-1.12/docker.zip"
+wget -outfile $env:TEMP\docker-cs-1.12.zip "https://download.docker.com/components/engine/windows-server/cs-1.12/docker-1.12.2.zip"
 Expand-Archive -Path $env:TEMP\docker-cs-1.12.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
 Remove-Item $env:TEMP\docker-cs-1.12.zip
