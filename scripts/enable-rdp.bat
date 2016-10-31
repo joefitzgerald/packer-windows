1c96bd6e6c743919269b6ced9613a35620ac7fa3
Jim Minter <jminter@redhat.com>
2016-10-31 15:47:39 +0000

    use profile=any for terminal server firewall and disable credssp

--- scripts/enable-rdp.bat
+++ scripts/enable-rdp.bat
@@ -1,2 +1,3 @@
-netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
+netsh advfirewall firewall add rule name="Open Port 3389" profile=any dir=in action=allow protocol=TCP localport=3389 enable=yes
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
