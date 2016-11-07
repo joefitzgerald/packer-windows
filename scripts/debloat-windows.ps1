228729184182ed3c9f1349fe1fa51e5276b2aaf7
Stefan Scherer <scherer_stefan@icloud.com>
2016-11-08 00:08:14 +0100

    Disable more services

--- scripts/debloat-windows.ps1
+++ scripts/debloat-windows.ps1
@@ -3,10 +3,18 @@ $url="https://github.com/StefanScherer/Debloat-Windows-10/archive/master.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, "$env:TEMP\debloat.zip")
 Expand-Archive -Path $env:TEMP\debloat.zip -DestinationPath $env:TEMP -Force
 
-Write-Host Debloating
+Write-Host Disable scheduled tasks
 . $env:TEMP\Debloat-Windows-10-master\utils\disable-scheduled-tasks.ps1
+Write-Host Block telemetry
 . $env:TEMP\Debloat-Windows-10-master\scripts\block-telemetry.ps1
+Write-Host Disable services
 . $env:TEMP\Debloat-Windows-10-master\scripts\disable-services.ps1
+Write-host Disable Windows Defender
 . $env:TEMP\Debloat-Windows-10-master\scripts\disable-windows-defender.ps1
+Write-host Optimize Windows Update
 . $env:TEMP\Debloat-Windows-10-master\scripts\optimize-windows-update.ps1
+Write-host Disable Windows Update
+set-service wuauserv -startuptype disabled
+set-service trustedinstaller -startuptype disabled
+Write-Host Remove OneDrive
 . $env:TEMP\Debloat-Windows-10-master\scripts\remove-onedrive.ps1
