1ad8d7dad5e477d2a9013262f059f5b7ba3bcb03
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-01 18:34:27 +0100

    Install docker 1.13.0-rc2, update base image to 447

--- scripts/docker/install-docker.ps1
+++ scripts/docker/install-docker.ps1
@@ -1,9 +1,10 @@
 Set-ExecutionPolicy Bypass -scope Process
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
-wget -outfile $env:TEMP\docker-cs-1.12.zip "https://download.docker.com/components/engine/windows-server/cs-1.12/docker-1.12.2.zip"
-Expand-Archive -Path $env:TEMP\docker-cs-1.12.zip -DestinationPath $env:TEMP -Force
+# wget -outfile $env:TEMP\docker-cs-1.12.zip "https://download.docker.com/components/engine/windows-server/cs-1.12/docker-1.12.2.zip"
+wget -outfile $env:TEMP\docker-1.13.0-rc2.zip "https://test.docker.com/builds/Windows/x86_64/docker-1.13.0-rc2.zip"
+Expand-Archive -Path $env:TEMP\docker-1.13.0-rc2.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
-Remove-Item $env:TEMP\docker-cs-1.12.zip
+Remove-Item $env:TEMP\docker-1.13.0-rc2.zip
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
 $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
 . dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
@@ -15,9 +16,9 @@ Write-Host "Fix --restart=always for reboot ..."
 Start-Service Docker
 
 Write-Host "Installing WindowsServerCore container image..."
-& "C:\Program Files\docker\docker.exe" pull microsoft/windowsservercore:10.0.14393.321
-& "C:\Program Files\docker\docker.exe" tag microsoft/windowsservercore:10.0.14393.321 microsoft/windowsservercore:latest
+& "C:\Program Files\docker\docker.exe" pull microsoft/windowsservercore:10.0.14393.447
+& "C:\Program Files\docker\docker.exe" tag microsoft/windowsservercore:10.0.14393.447 microsoft/windowsservercore:latest
 
 Write-Host "Installing NanoServer container image..."
-& "C:\Program Files\docker\docker.exe" pull microsoft/nanoserver:10.0.14393.321
-& "C:\Program Files\docker\docker.exe" tag microsoft/nanoserver:10.0.14393.321 microsoft/nanoserver:latest
+& "C:\Program Files\docker\docker.exe" pull microsoft/nanoserver:10.0.14393.447
+& "C:\Program Files\docker\docker.exe" tag microsoft/nanoserver:10.0.14393.447 microsoft/nanoserver:latest
