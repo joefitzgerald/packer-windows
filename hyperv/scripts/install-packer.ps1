3a04c36ae985886776ba1fb8161312f15a82feff
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-03 09:30:23 +0100

    Do not change directory

--- hyperv/scripts/install-packer.ps1
+++ hyperv/scripts/install-packer.ps1
@@ -6,6 +6,6 @@ if (Test-Path $env:TEMP\packer) {
   rm -recurse -force $env:TEMP\packer
 }
 wget -outfile $env:TEMP\packer.zip -uri https://dl.bintray.com/taliesins/Packer/Packer.1.0.0.145-HyperV.nupkg -UseBasicParsing
-Expand-Archive $env:TEMP\packer.zip -DestinationPath $env:TEMP
+Expand-Archive $env:TEMP\packer.zip -DestinationPath $env:TEMP\packer
 copy $env:TEMP\packer\packer.exe $env:ChocolateyInstall\bin\packer.exe
 rm -force $env:TEMP\packer.zip
