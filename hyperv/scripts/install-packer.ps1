2c6b97fd56ca3939a988d305a8b2f741f44e6604
Stefan Scherer <scherer_stefan@icloud.com>
2016-12-03 09:27:36 +0100

    Repeatable download

--- hyperv/scripts/install-packer.ps1
+++ hyperv/scripts/install-packer.ps1
@@ -1,9 +1,12 @@
 cd $env:TEMP
 $ProgressPreference = 'SilentlyContinue'
 if (Test-Path packer.zip) {
-  rm packer.zip
+  rm -force packer.zip
+}
+if (Test-Path packer) {
+  rm -recurse -force packer
 }
 wget -outfile packer.zip -uri https://dl.bintray.com/taliesins/Packer/Packer.1.0.0.145-HyperV.nupkg -UseBasicParsing
 Expand-Archive packer.zip
 copy packer\packer.exe $env:ChocolateyInstall\bin\packer.exe
-rm packer.zip
+rm -force packer.zip
