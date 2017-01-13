37bb40bacd433b4604942bd8445376b690fce4f9
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-13 16:16:33 +0100

    Update build scripts

--- build_windows_2016_docker.ps1
+++ build_windows_2016_docker.ps1
@@ -4,8 +4,4 @@
 #       --var iso_checksum=99fd8082a609997ae97a514dca22becf20420891 `
 #       --var autounattend=./tmp/10/Autounattend.xml `
 #       windows_10.json
-
-# bigger disk for hyperv
-packer build --only=vmware-iso `
-       --var disk_size=136400 `
-       windows_10.json
+packer build windows_2016_docker.json
