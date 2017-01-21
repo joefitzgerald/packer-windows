d026e2ea3fd199bd48c850ca8214e652145515fb
Stefan Scherer <scherer_stefan@icloud.com>
2017-01-21 18:46:02 +0100

    Use Start-Job with -ArgumentList
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- scripts/docker/docker-pull-async.ps1
+++ scripts/docker/docker-pull-async.ps1
@@ -2,7 +2,7 @@ function DockerPull {
   Param ([string]$image)
 
   Write-Host Installing $image ...
-  $j = Start-Job -ScriptBlock { docker pull $image }
+  $j = Start-Job -ScriptBlock { docker pull $args[0] } -ArgumentList $image
   while ( $j.JobStateInfo.state -ne "Completed" ) {
     Write-Host $j.JobStateInfo.state
     Start-Sleep 10
