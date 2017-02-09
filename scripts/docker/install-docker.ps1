9474b83cc8aa85099c7a0c25d9ad685a2243d957
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-09 08:12:43 +0100

    Update Docker 1.13.1

--- scripts/docker/install-docker.ps1
+++ scripts/docker/install-docker.ps1
@@ -1,10 +1,10 @@
 Set-ExecutionPolicy Bypass -scope Process
 New-Item -Type Directory -Path "$($env:ProgramFiles)\docker"
 # wget -outfile $env:TEMP\docker-cs-1.12.zip "https://download.docker.com/components/engine/windows-server/cs-1.12/docker-1.12.2.zip"
-wget -outfile $env:TEMP\docker-1.13.0.zip "https://get.docker.com/builds/Windows/x86_64/docker-1.13.0.zip"
-Expand-Archive -Path $env:TEMP\docker-1.13.0.zip -DestinationPath $env:TEMP -Force
+wget -outfile $env:TEMP\docker-1.13.1.zip "https://get.docker.com/builds/Windows/x86_64/docker-1.13.1.zip"
+Expand-Archive -Path $env:TEMP\docker-1.13.1.zip -DestinationPath $env:TEMP -Force
 copy $env:TEMP\docker\*.exe $env:ProgramFiles\docker
-Remove-Item $env:TEMP\docker-1.13.0.zip
+Remove-Item $env:TEMP\docker-1.13.1.zip
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$($env:ProgramFiles)\docker", [EnvironmentVariableTarget]::Machine)
 $env:Path = $env:Path + ";$($env:ProgramFiles)\docker"
 . dockerd --register-service -H npipe:// -H 0.0.0.0:2375 -G docker
