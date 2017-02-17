4e9d38fbadae184969901bdc1899b6695ba733be
Stefan Scherer <scherer_stefan@icloud.com>
2017-02-17 23:12:16 +0100

    Update Windows 10 Docker build

--- build_windows_10_docker.sh
+++ build_windows_10_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Windows 10 Insider 15031 + Docker 1.13.1
-packer build --only=vmware-vmx --var source_path=~/.vagrant.d/boxes/windows_10_insider_15031/0/vmware_desktop/packer-vmware-iso.vmx windows_10_docker.json
+packer build --only=vmware-vmx --var source_path=~/.vagrant.d/boxes/windows_10/0/vmware_desktop/windows_10.vmx windows_10_docker.json
