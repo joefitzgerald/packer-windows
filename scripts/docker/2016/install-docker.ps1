f5d4a5eb23b68ff63f6a9c68488a3922c7058f32
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-25 16:22:57 +0200

    Remove some old output

--- scripts/docker/2016/install-docker.ps1
+++ scripts/docker/2016/install-docker.ps1
@@ -1,14 +1,9 @@
-Get-PackageSource
 Write-Host "Install-PackageProvider ..."
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Get-PackageSource
 Write-Host "Install-Module ..."
 Install-Module -Name DockerMsftProviderInsider -Force
-Get-PackageSource
-Write-Host "Set-PSRepository trusted ..."
-Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
 Write-Host "Install-Package ..."
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
 Install-Package -Name docker -ProviderName DockerMsftProviderInsider -Force
-Write-Host "Set-PSRepository untrusted ..."
 Set-PSRepository -InstallationPolicy Untrusted -Name PSGallery
 Start-Service docker
