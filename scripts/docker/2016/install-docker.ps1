90f275525e80b88aab617309c4b49a33db6298c8
Stefan Scherer <scherer_stefan@icloud.com>
2017-08-09 13:25:36 +0200

    Install 17.06 CE

--- scripts/docker/2016/install-docker.ps1
+++ scripts/docker/2016/install-docker.ps1
@@ -4,6 +4,6 @@ Write-Host "Install-Module ..."
 Install-Module -Name DockerMsftProviderInsider -Force
 Write-Host "Install-Package ..."
 Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
-Install-Package -Name docker -ProviderName DockerMsftProviderInsider -Force
+Install-Package -Name docker -ProviderName DockerMsftProviderInsider -Force -RequiredVersion 17.06.0-ce
 Set-PSRepository -InstallationPolicy Untrusted -Name PSGallery
 Start-Service docker
