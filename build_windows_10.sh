631ff796e97ee02f531cced9cd92ffe95dd94935
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-02 15:27:10 +0200

    Update insider

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -4,10 +4,8 @@
 # Windows 10 Pro own license
 #packer build --only=vmware-iso --var iso_url=~/packer_cache/my/Win10_1607_English_x64.iso --var iso_checksum=99fd8082a609997ae97a514dca22becf20420891 --var autounattend=./tmp/10_pro/Autounattend.xml windows_10.json
 
-# Windows 10 Insider 16232
-# packer build --only=vmware-iso --var iso_url=~/packer_cache/insider/Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16232.iso --var iso_checksum=8e84a14b972a2d7643d347c57068b51e88e86ae5 windows_10_insider.json
-# Windows 10 Insider 16241
-packer build --only=vmware-iso --var iso_url=~/packer_cache/connect/16241.1001.170708-1800.rs_prerelease_CLIENTENTERPRISE_VOL_X64FRE_en-US.iso --var iso_checksum=1ef19500cd45ba6bd013c4a0c3fd1633a2f830c6 windows_10_insider.json
+# Windows 10 Insider
+packer build --only=vmware-iso --var iso_url=~/packer_cache/connect/16281.1000.170829-1438.rs3_release_CLIENT_BUSINESS_x64FRE_en-us.iso --var iso_checksum=1424eee844683d5e0206f94a034f3ddb80f13f65add5bf838c8608f247a99bd9 windows_10_insider.json
 
 # Windows 10 Enterprise MSDN
 #packer build --only=vmware-iso --var iso_url=~/packer_cache/msdn/en_windows_10_enterprise_version_1607_updated_jan_2017_x64_dvd_9714415.iso --var iso_checksum=97164DD5C1C933BAEF89A4BDE93D544256134FE4 --var iso_checksum_type=sha1 --var autounattend=./tmp/10/Autounattend.xml windows_10.json
