e505d43c8bfece8a6f5f339a365b6bb224fbf3b7
Stefan Scherer <scherer_stefan@icloud.com>
2017-10-25 21:45:08 +0200

    Build preallocated disks

--- build_windows_server_1709.sh
+++ build_windows_server_1709.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # MSDN 1709 ISO
-packer build --only=virtualbox-iso --var iso_url=~/packer_cache/msdn/en_windows_server_version_1709_x64_dvd_100090904.iso --var iso_checksum=ca1108d5be2c091bfb57e8f3db3be1e8baa9c32802131f7a6e43e63f7b596591 --var iso_checksum_type=sha256 --var autounattend=./tmp/1709/Autounattend.xml windows_server_1709.json
+packer build \
+  --only=vmware-iso \
+  --var disk_type_id=3 \
+  --var disk_size=30720 \
+  --var vhv_enable=true \
+  --var iso_url=~/packer_cache/msdn/en_windows_server_version_1709_x64_dvd_100090904.iso \
+  --var iso_checksum=ca1108d5be2c091bfb57e8f3db3be1e8baa9c32802131f7a6e43e63f7b596591 \
+  --var iso_checksum_type=sha256 \
+  --var autounattend=./tmp/1709/Autounattend.xml \
+  windows_server_1709.json
