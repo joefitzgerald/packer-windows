3e35d1588be2d947aa404075caa9d093b91f3cc7
Noxx <Flauschbaellchen@users.noreply.github.com>
2017-11-05 15:37:23 +0100

    Simplified certificate installation for virtualbox
    As commented on https://github.com/joefitzgerald/packer-windows/pull/247#issuecomment-326535770

--- scripts/vm-guest-tools.bat
+++ scripts/vm-guest-tools.bat
@@ -34,9 +34,7 @@ cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
 
 move /Y C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
 cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox"
-certutil -addstore -f "TrustedPublisher" C:\Windows\Temp\virtualbox\cert\vbox-sha256-r3.cer
-certutil -addstore -f "TrustedPublisher" C:\Windows\Temp\virtualbox\cert\vbox-sha256.cer
-certutil -addstore -f "TrustedPublisher" C:\Windows\Temp\virtualbox\cert\vbox-sha1.cer
+cmd /c for %%i in (C:\Windows\Temp\virtualbox\cert\vbox*.cer) do C:\Windows\Temp\virtualbox\cert\VBoxCertUtil add-trusted-publisher %%i --root %%i
 cmd /c C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe /S
 goto :done
 
