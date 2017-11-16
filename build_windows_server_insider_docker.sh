b95de4ff93377c8b847a85c1347618b23f68dc32
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-16 20:14:33 +0100

    Update Server Insider builds

---
+++ build_windows_server_insider_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Microsoft Connect ISO
+# packer build --only=vmware-iso --var iso_url=~/packer_cache/connect/17035.1000.171103-1616.rs_prerelease_SERVER_ACORE_VOL_x64FRE_en-us.iso --var iso_checksum=dd412bd5c7d29fa5166a10f4cb36dacf7fcb605a5b8e8a6e8fa87e13aed6851d --var iso_checksum_type=sha256 --var autounattend=./tmp/2016_connect/Autounattend.xml  windows_server_insider_docker.json
+
+# Windows Insider ISO
+packer build \
+  --only=vmware-iso \
+  --var disk_type_id=3 \
+  --var disk_size=30720 \
+  --var vhv_enable=true \
+  --var iso_url=~/packer_cache/insider/Windows_InsiderPreview_Server_17035.iso \
+  windows_server_insider_docker.json
--- scripts/docker/docker-pull-local.ps1
+++ scripts/docker/docker-pull-local.ps1
@@ -5,7 +5,7 @@
 #}
 
 Write-Host "Downloading nanoserver image"
-docker import http://172.16.63.1:8080/CBaseOs_rs3_release_16278.1000.170825-1441_amd64fre_NanoServer_en-us.tar.gz microsoft/nanoserver-insider:latest
-Write-Host "Downloading windowsservercore image"
-docker import http://172.16.63.1:8080/CBaseOs_rs3_release_16278.1000.170825-1441_amd64fre_ServerDatacenterCore_en-us.tar.gz microsoft/windowsservercore-insider:latest
+docker import http://172.16.236.1:8080/CBaseOs_rs_prerelease_17035.1000.171103-1616_amd64fre_NanoServer_en-us.tar.gz microsoft/nanoserver-insider:latest
+#Write-Host "Downloading windowsservercore image"
+#docker import http://172.16.236.1:8080/CBaseOs_rs_prerelease_17035.1000.171103-1616_amd64fre_ServerDatacenterCore_en-us.tar.gz microsoft/windowsservercore-insider:latest
 docker images
