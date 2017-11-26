57eb0af159e03ff4f9515cba5bce8ba9b92321f6
Stefan Scherer <scherer_stefan@icloud.com>
2017-11-26 20:22:39 +0100

    Disable progress

--- nested/scripts/install-tools.ps1
+++ nested/scripts/install-tools.ps1
@@ -1,9 +1,8 @@
 # install chocolatey
 iex (wget 'https://chocolatey.org/install.ps1' -UseBasicParsing)
+choco feature disable --name showDownloadProgress
 
 choco install -y packer
-
 choco install -y vagrant
-
 choco install -y docker
 choco install -y docker-machine
