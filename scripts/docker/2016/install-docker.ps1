e7df8f5b9c18f2a44d8718a2a446b575f2a14442
Stefan Scherer <scherer_stefan@icloud.com>
2017-12-09 18:43:50 +0100

    Refactor install-docker and docker-pull

--- scripts/docker/2016/install-docker.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Install-PackageProvider ..."
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Write-Host "Install-Module ..."
-Install-Module -Name DockerMsftProviderInsider -Force
-Write-Host "Install-Package ..."
-Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
-Install-Package -Name docker -ProviderName DockerMsftProviderInsider -Force
-Set-PSRepository -InstallationPolicy Untrusted -Name PSGallery
-Start-Service docker
