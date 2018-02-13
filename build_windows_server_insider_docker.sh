3ba8e81afc682e243d7622acdaa72a16f650ef4e
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-13 23:50:28 +0100

    Update Windows Server Insider 17093

--- build_windows_server_insider_docker.sh
+++ build_windows_server_insider_docker.sh
@@ -4,5 +4,5 @@
 PACKER_LOG=debug packer build \
   --only=vmware-iso \
   --var vhv_enable=true \
-  --var iso_url=~/packer_cache/insider/Windows_InsiderPreview_Server_17079.iso \
+  --var iso_url=~/packer_cache/insider/Windows_InsiderPreview_Server_17093.iso \
   windows_server_insider_docker.json
