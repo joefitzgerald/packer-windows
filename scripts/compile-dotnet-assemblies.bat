8f79f50d6186207ffb1473e000eed86c054ceb04
Stefan Scherer <scherer_stefan@icloud.com>
2018-02-14 00:05:10 +0100

    Re-add compile-dotnet-assemblies.bat

--- scripts/compile-dotnet-assemblies.bat
+++ scripts/compile-dotnet-assemblies.bat
@@ -6,10 +6,12 @@ if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto 64BIT
 %windir%\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue
 %windir%\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems
 
-exit /b
+exit 0
 
 :64BIT
 %windir%\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue
 %windir%\microsoft.net\framework64\v4.0.30319\ngen.exe update /force /queue
 %windir%\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems
 %windir%\microsoft.net\framework64\v4.0.30319\ngen.exe executequeueditems
+
+exit 0
