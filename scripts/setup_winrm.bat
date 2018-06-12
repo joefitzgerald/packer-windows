034dc0af571eacaff92e25053199d1a7ba085a8d
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-06-12 15:50:33 +0300

    use winrm quickconfig instead of powershell

--- scripts/setup_winrm.bat
+++ scripts/setup_winrm.bat
@@ -1,11 +1,14 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Enable-PSRemoting -Force"
-
+cmd.exe /c winrm quickconfig -q
+cmd.exe /c winrm quickconfig -transport:http
 cmd.exe /c winrm set winrm/config @{MaxTimeoutms="1800000"}
 cmd.exe /c winrm set winrm/config/winrs @{MaxConcurrentUsers="100"}
 cmd.exe /c winrm set winrm/config/winrs @{MaxProcessesPerShell="100"}
 cmd.exe /c winrm set winrm/config/winrs @{MaxMemoryPerShellMB="1024"}
 cmd.exe /c winrm set winrm/config/winrs @{MaxShellsPerUser="100"}
 cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
+cmd.exe /c winrm set winrm/config/client @{AllowUnencrypted="true"}
 cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/client/auth @{Basic="true"}
+cmd.exe /c winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"}
 
-REG add "HKLM\SYSTEM\CurrentControlSet\services\WinRM" /v DelayedAutostart /t REG_DWORD /d 0 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\services\WinRM" /v DelayedAutostart /t REG_DWORD /d 0 /f
